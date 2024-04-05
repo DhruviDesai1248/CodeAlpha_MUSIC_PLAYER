4d4f359bb5bc2bf6733097c044f7bed0f8d35297
DhruviDesai1248 <desaidhruvi970@gmail.com>
2024-04-05 23:03:16 +0530

    first commit

---
+++ src/components/fragment/Navigation.jsx
@@ -0,0 +1,38 @@
+import React, {useContext, useState} from "react";
+import '../assets/scss/Navigation.scss';
+import SearchBar from "./SearchBar";
+import Brand from "./Brand";
+import {ThemeContext} from "../../api/Theme";
+
+function Navigation() {
+
+    const [isLanguageListOpen, setLangList] = useState(false);
+    const [isOpenProfile, setOpenProfile] = useState(false);
+
+
+    function handleOpenLanguageList() {
+        if (isOpenProfile === true)
+            setOpenProfile(!isOpenProfile);
+        setLangList(!isLanguageListOpen);
+    }
+
+    function handleOpenProfile() {
+        if (isLanguageListOpen === true)
+            setLangList(!isLanguageListOpen);
+        setOpenProfile(!isOpenProfile);
+    }
+    const useStyle = useContext(ThemeContext);
+    return (
+        <nav style={useStyle.component}>
+            <Brand/>
+            <div className={"navigation"}>
+               {/* <NavigationButton href={"/home"} name={"Home"}/>*/}
+               {/* <NavigationButton href={"/home/about"} name={"About"}/>*/}
+                {/*<NavigationButton href={"/home/add"} name={"Add"}/>*/}
+            </div>
+            <SearchBar/>
+        </nav>
+    );
+}
+
+export default Navigation;
---
+++ src/db/music.js
@@ -0,0 +1,180 @@
+let musicDB = [
+    {
+        id: 0,
+        name: "Apna Bana Le ",
+        author_name: "Arijit Singh",
+        img: "M1_Bhediya.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "M1_Apna Bana Le - Bhediya 320 Kbps.mp3",
+        attribution: {
+            song: "Apna Bana Le",
+            musicBy: "Arijit Singh",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 1,
+        name: "Badri Ki Dulhania",
+        author_name: "Karan Johar",
+        img: "M2_Badrinath-Ki-Dulhania.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type:"instrumental",
+        musicName: "M2_Badri Ki Dulhania (Title Track)-(PagalSongs.Com.IN).mp3",
+        attribution: {
+            song: "Badri Ki Dulhania",
+            musicBy: "Karan Johar",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 2,
+        name: "Kooch Na Karin",
+        author_name: "Azhar Abbas",
+        img: "M3_Load-Wedding.jpg",
+        lang: null,
+        type: "electronic",
+        timesPlayed: 0,
+        musicName: "M3_Kooch Na Karin_320(Ghantalele.com).mp3",
+        attribution: {
+            song: "Kooch Na Karin",
+            musicBy: "Azhar Abbas",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 3,
+        name: "Pehla Nasha",
+        author_name: "Various",
+        img: "M4_pehla-nasha.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "M4_Pehla Nasha (Instrumental)-(PagalSongs.Com.IN).mp3",
+        attribution: {
+            song: "Pehla Nasha",
+            musicBy: "Various",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 4,
+        name: "Pretty Woman",
+        author_name: "Shankar",
+        img: "M5_Kal-Ho-Naa-Ho.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "M5_Pretty Woman - Kal Ho Naa Ho 320 Kbps.mp3",
+        attribution: {
+            song: "Pretty Woman",
+            musicBy: "Shankar",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 5,
+        name: "Raataan Lambiyan",
+        author_name: "Jubin Nautiyal",
+        img: "M6_Raataan-Lambiyan.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "M6_Raataan Lambiyan-(PagalSongs.Com.IN).mp3",
+        attribution: {
+            song: "Raataan Lambiyan",
+            musicBy: "Jubin Nautiyal",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 6,
+        name: "Tere Naina",
+        author_name: "Amanat Ali",
+        img: "M7_My-Name-Is-Khan.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type:"instrumental",
+        musicName: "M7_Tere Naina_192(Ghantalele.com).mp3",
+        attribution: {
+            song: "Tere Naina",
+            musicBy: "Amanat Ali",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 7,
+        name: "Teri Baaton Mein",
+        author_name: "Raghav",
+        img: "M8_teri-baaton.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type:"instrumental",
+        musicName: "M8_Teri Baaton Mein Aisa Uljha Jiya-(PagalSongs.Com.IN).mp3",
+        attribution: {
+            song: "M8_Teri Baaton Mein Aisa Uljha Jiya-(PagalSongs.Com.IN)",
+            musicBy: "Raghav",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 8,
+        name: "Channa Mereya",
+        author_name: "Pritam",
+        img: "M9_Channa-Mereya.jpg",
+        lang: null,
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "M9_Channa Mereya Ae Dil Hai Mushkil 128 Kbps.mp3",
+        attribution: {
+            song: "M9_Channa Mereya Ae Dil Hai Mushkil 128 Kbps",
+            musicBy: "Pritam",
+            download: null,
+            stream: null
+        }
+    },
+    {
+      id: 9,
+      name: "I Love You",
+      author_name: "Pritam",
+      img: "M10_Bodyguard.jpg",
+      lang: null,
+      timesPlayed: 0,
+      type: "electronic",
+      musicName: "M10_I Love You_320(Ghantalele.com).mp3",
+      attribution: {
+          song: "I Love You",
+          musicBy: "Ash King",
+          download: null,
+          stream: null
+      }
+    },
+    {
+      id: 10,
+      name: "Hum Nashe Mein Toh Nahin",
+      author_name: "Pritam",
+      img: "M11_Hum-Nashe-Mein.jpg",
+      lang: null,
+      timesPlayed: 0,
+      type: "electronic",
+      musicName: "M11_Hum Nashe Mein Toh Nahin.mp3",
+      attribution: {
+          song: "um Nashe Mein Toh Nahin",
+          musicBy: "Pritam",
+          download: null,
+          stream: null
+      }
+    }
+];
+
+export default musicDB;
